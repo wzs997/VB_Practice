bee4eb896c78297e10d013ff7b3debbe34cf313c
wzs997 <wzs_997@126.com>
2018-11-10 23:44:03 +0800

    aaa

---
+++ 计算器/Form1.Designer.vb
@@ -0,0 +1,307 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(234, 200)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(37, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "9"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(116, 200)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(37, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "7"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(175, 200)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(37, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "8"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(175, 171)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(37, 23)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "5"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(116, 171)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(37, 23)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "4"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(234, 171)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(37, 23)
+        Me.Button6.TabIndex = 3
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(175, 142)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(37, 23)
+        Me.Button7.TabIndex = 8
+        Me.Button7.Text = "2"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(116, 142)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(37, 23)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "1"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(234, 142)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(37, 23)
+        Me.Button9.TabIndex = 6
+        Me.Button9.Text = "3"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(315, 164)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(62, 59)
+        Me.Button10.TabIndex = 10
+        Me.Button10.Text = "计算"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(110, 58)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(43, 21)
+        Me.TextBox1.TabIndex = 11
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(169, 58)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(43, 21)
+        Me.TextBox2.TabIndex = 12
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(228, 58)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(43, 21)
+        Me.TextBox3.TabIndex = 13
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(322, 96)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(56, 21)
+        Me.TextBox4.TabIndex = 14
+        Me.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(335, 81)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 12)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "结果"
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(116, 275)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(37, 23)
+        Me.Button13.TabIndex = 17
+        Me.Button13.Text = "+"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(175, 275)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(37, 23)
+        Me.Button12.TabIndex = 18
+        Me.Button12.Text = "-"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(234, 275)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(37, 23)
+        Me.Button11.TabIndex = 16
+        Me.Button11.Text = "*"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(116, 304)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(37, 23)
+        Me.Button14.TabIndex = 19
+        Me.Button14.Text = "/"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(175, 229)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(37, 23)
+        Me.Button15.TabIndex = 20
+        Me.Button15.Text = "0"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(315, 244)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(62, 23)
+        Me.Button16.TabIndex = 21
+        Me.Button16.Text = "清空"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Location = New System.Drawing.Point(315, 287)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(62, 23)
+        Me.Button17.TabIndex = 22
+        Me.Button17.Text = "退格"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(403, 350)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button13 As System.Windows.Forms.Button
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button14 As System.Windows.Forms.Button
+    Friend WithEvents Button15 As System.Windows.Forms.Button
+    Friend WithEvents Button16 As System.Windows.Forms.Button
+    Friend WithEvents Button17 As System.Windows.Forms.Button
+
+End Class
---
+++ 计算器/Form1.vb
@@ -0,0 +1,139 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        If TextBox1.Text = "" Then
+            TextBox1.Text = "1"
+        Else
+            TextBox3.Text = "1"
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        If TextBox1.Text = "" Then
+            TextBox1.Text = "2"
+        Else
+            TextBox3.Text = "2"
+        End If
+    End Sub
+
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+        If TextBox1.Text = "" Then
+            TextBox1.Text = "3"
+        Else
+            TextBox3.Text = "3"
+        End If
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        If TextBox1.Text = "" Then
+            TextBox1.Text = "4"
+        Else
+            TextBox3.Text = "4"
+        End If
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        If TextBox1.Text = "" Then
+            TextBox1.Text = "5"
+        Else
+            TextBox3.Text = "5"
+        End If
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        If TextBox1.Text = "" Then
+            TextBox1.Text = "6"
+        Else
+            TextBox3.Text = "6"
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If TextBox1.Text = "" Then
+            TextBox1.Text = "7"
+        Else
+            TextBox3.Text = "7"
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        If TextBox1.Text = "" Then
+            TextBox1.Text = "8"
+        Else
+            TextBox3.Text = "8"
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Then
+            TextBox1.Text = "9"
+        Else
+            TextBox3.Text = "9"
+        End If
+    End Sub
+
+    Private Sub Button13_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button13.Click
+        TextBox2.Text = "+"
+    End Sub
+
+    Private Sub Button12_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button12.Click
+        TextBox2.Text = "-"
+    End Sub
+
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        TextBox2.Text = "*"
+    End Sub
+
+    Private Sub Button14_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button14.Click
+        TextBox2.Text = "/"
+    End Sub
+
+    Private Sub Button15_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button15.Click
+        If TextBox1.Text = "" Then
+            TextBox1.Text = "0"
+        Else
+            TextBox3.Text = "0"
+        End If
+    End Sub
+
+    Private Sub Button16_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button16.Click
+        TextBox1.Text = ""
+        TextBox3.Text = ""
+        TextBox2.Text = ""
+    End Sub
+
+    Private Sub Button17_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button17.Click
+        If Not TextBox3.Text = "" Then
+            TextBox3.Text = ""
+        ElseIf Not TextBox2.Text = "" Then
+            TextBox2.Text = ""
+        ElseIf Not TextBox1.Text = "" Then
+            TextBox1.Text = ""
+        End If
+    End Sub
+
+    Private Sub TextBox4_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox4.TextChanged
+   
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+        Select Case TextBox2.Text
+            Case "+"
+                TextBox4.Text = (TextBox1.Text) + Val(TextBox3.Text)
+            Case "-"
+                TextBox4.Text = (TextBox1.Text) - Val(TextBox3.Text)
+            Case "*"
+                TextBox4.Text = (TextBox1.Text) * Val(TextBox3.Text)
+            Case "/"
+                TextBox4.Text = (TextBox1.Text) / Val(TextBox3.Text)
+        End Select
+    End Sub
+End Class
