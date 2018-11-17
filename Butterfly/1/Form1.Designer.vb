9eb3b2d858397bd680866e19daeedf3a532b0c49
wzs997 <wzs_997@126.com>
2018-11-17 22:37:36 +0800

    butter fly_done

---
+++ Butterfly/1/Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.right_move = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.BelingBeling1 = New System.Windows.Forms.Timer(Me.components)
+        Me.left_move = New System.Windows.Forms.Timer(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.top_move = New System.Windows.Forms.Timer(Me.components)
+        Me.down_move = New System.Windows.Forms.Timer(Me.components)
+        Me.crash = New System.Windows.Forms.Timer(Me.components)
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(352, 167)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(352, 167)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'right_move
+        '
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(88, 44)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(39, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'BelingBeling1
+        '
+        '
+        'left_move
+        '
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 44)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(39, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(49, 73)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(39, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(49, 12)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(39, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'top_move
+        '
+        '
+        'down_move
+        '
+        '
+        'crash
+        '
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(738, 3)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(54, 52)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(738, 61)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(54, 52)
+        Me.Button6.TabIndex = 7
+        Me.Button6.Text = "Button6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(804, 378)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents right_move As System.Windows.Forms.Timer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents BelingBeling1 As System.Windows.Forms.Timer
+    Friend WithEvents left_move As System.Windows.Forms.Timer
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents top_move As System.Windows.Forms.Timer
+    Friend WithEvents down_move As System.Windows.Forms.Timer
+    Friend WithEvents crash As System.Windows.Forms.Timer
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+
+End Class
---
+++ Butterfly/1/Form1.vb
@@ -0,0 +1,106 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        right_move.Interval = 500
+        left_move.Interval = 500
+        top_move.Interval = 500
+        down_move.Interval = 500
+        crash.Interval = 100
+        crash.Enabled = True
+        BelingBeling1.Interval = 300
+        PictureBox1.Visible = False
+        Button1.Text = "右移"
+        Button2.Text = "左移"
+        Button4.Text = "上移"
+        Button3.Text = "下移"
+        PictureBox1.Size = New Size(80, 80)
+        PictureBox2.Size = New Size(80, 80)
+        PictureBox1.Image = System.Drawing.Image.FromFile("../fly2.png")
+        PictureBox2.Image = System.Drawing.Image.FromFile("../fly1.png")
+        Me.BackColor = Color.White
+        Button5.Text = "快速"
+        Button6.Text = "慢速"
+    End Sub
+    Dim P_right_move%
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles right_move.Tick '向右移
+        P_right_move = 6
+        PictureBox1.Location = New Point(PictureBox1.Location.X + P_right_move, PictureBox1.Location.Y)
+        PictureBox2.Location = New Point(PictureBox2.Location.X + P_right_move, PictureBox2.Location.Y)
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click '向右移__定时器
+        right_move.Enabled = Not right_move.Enabled
+        left_move.Enabled = False
+        top_move.Enabled = False
+        down_move.Enabled = False
+        BelingBeling1.Enabled = True
+    End Sub
+
+    Private Sub Timer1_Tick_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BelingBeling1.Tick '闪烁
+        PictureBox2.Visible = Not PictureBox2.Visible
+        PictureBox1.Visible = Not PictureBox1.Visible
+    End Sub
+    Dim P_left_move%
+    Private Sub left_move_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles left_move.Tick '向左移
+        P_left_move = 6
+        PictureBox1.Location = New Point(PictureBox1.Location.X - P_left_move, PictureBox1.Location.Y)
+        PictureBox2.Location = New Point(PictureBox2.Location.X - P_left_move, PictureBox2.Location.Y)
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click '向左移__定时器
+        left_move.Enabled = Not left_move.Enabled
+        right_move.Enabled = False
+        top_move.Enabled = False
+        down_move.Enabled = False
+        BelingBeling1.Enabled = True
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click '向上移
+        top_move.Enabled = Not top_move.Enabled
+        right_move.Enabled = False
+        left_move.Enabled = False
+        down_move.Enabled = False
+        BelingBeling1.Enabled = True
+    End Sub
+    Dim P_top_move%
+    Private Sub top_move_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles top_move.Tick '向上移__定时器
+        P_top_move = 6
+        PictureBox1.Location = New Point(PictureBox1.Location.X, PictureBox1.Location.Y - P_top_move)
+        PictureBox2.Location = New Point(PictureBox2.Location.X, PictureBox2.Location.Y - P_top_move)
+    End Sub
+    Dim P_down_move%
+    Private Sub down_move_Tick(sender As System.Object, e As System.EventArgs) Handles down_move.Tick '向下移_定时器
+        P_down_move = 6
+        PictureBox1.Location = New Point(PictureBox1.Location.X, PictureBox1.Location.Y + P_down_move)
+        PictureBox2.Location = New Point(PictureBox2.Location.X, PictureBox2.Location.Y + P_down_move)
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click '向下移
+        down_move.Enabled = Not down_move.Enabled
+        right_move.Enabled = False
+        left_move.Enabled = False
+        top_move.Enabled = False
+        BelingBeling1.Enabled = True
+    End Sub
+
+    Private Sub crash_Tick(sender As System.Object, e As System.EventArgs) Handles crash.Tick
+        If PictureBox1.Location.X < 0 Or PictureBox1.Location.X > Me.Size.Width - PictureBox1.Size.Width Or
+          PictureBox1.Location.Y < 0 Or PictureBox1.Location.Y > Me.Size.Height - PictureBox1.Size.Height Then
+            PictureBox1.Location = New Point(Rnd() * Me.Size.Width - PictureBox1.Size.Width, Rnd() * Me.Size.Height - PictureBox1.Size.Height)
+            PictureBox2.Location = PictureBox1.Location
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
+        right_move.Interval = 100
+        left_move.Interval = 100
+        top_move.Interval = 100
+        down_move.Interval = 100
+    End Sub
+
+    Private Sub Button6_Click(sender As System.Object, e As System.EventArgs) Handles Button6.Click
+        right_move.Interval = 1000
+        left_move.Interval = 1000
+        top_move.Interval = 1000
+        down_move.Interval = 1000
+    End Sub
+End Class
